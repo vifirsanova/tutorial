06e1e110a49630faac9d23827c1a4f2ff4d31b1f
mssvctr <70071046+vifirsanova@users.noreply.github.com>
2025-07-09 10:36:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const tabButtons = document.querySelectorAll('.tab-button');
     tabButtons.forEach(button => {
         button.addEventListener('click', () => {
-            // Remove active class from all buttons and content
             document.querySelectorAll('.tab-button').forEach(btn => btn.classList.remove('active'));
             document.querySelectorAll('.tab-content').forEach(content => content.classList.remove('active'));
             
